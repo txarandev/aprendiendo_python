2b81d202f3263a638db45bdf5c5d34d8066d91ca
txarandev <programmingtxaran@gmail.com>
2026-02-01 22:15:01 +0100

    empezamos con las variables y trabajamos con ellas

---
+++ 04_variables.py
@@ -0,0 +1,24 @@
+#creando una variable 
+name = "Pepito"
+print(name)
+print("mi nombre es?? " + name)
+
+a = 3
+b = 6
+c = 7
+
+
+
+#modificando el valor de una variable.
+d = c + 2
+
+
+#usando variables para conversiones
+kilometers = 50
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
