fd072405ede1dddb4bb6329e9161fa3d8b1feb08
txarandev <programmingtxaran@gmail.com>
2026-02-01 18:37:52 +0100

    añadiendo y modificando algunos archivos

---
+++ 02_literales_python.py
@@ -0,0 +1,26 @@
+#tipos de datos
+
+#tipo STRING, cadenas texto.
+print("2")
+
+#tipo INT, enteros.
+print(2)
+
+#tipo float, flotantes(decimales).
+print(1.45)
+#La letra 'e o E' proviene de la palabra 'exponente', la cual significa por diez a la potencia:
+#El exponente (el valor después de la E) debe ser un valor entero.
+#La base (el valor antes de la E) puede ser un valor entero o flotante.
+print(3E8) # imprime 300000000
+#codificando flotantes:
+print( 0.0000000000000000001) #imprime la potencia 1e-19
+
+#PARA ESTOS DOS TIPOS MIRAR info_octal_hexa.txt
+#tipo octal '0o' y solo debe tener dijitos del [0 al 7]. Número octal con un valor (decimal) 
+print(0o123) #imprime 83
+
+#tipo hexadecimal '0x' con un valor en (decimal).
+print(0x123) #imprime 291
+
+
+
