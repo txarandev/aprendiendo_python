b5257dcda72af7e2381b4d27f1673930d4640af7
txarandev <programmingtxaran@gmail.com>
2026-02-01 21:41:28 +0100

    añadiendo operadores

---
+++ 03_operadores.py
@@ -0,0 +1,36 @@
+#Operadores Basicos
+
+#exponenciacion: '**'
+print(2 ** 3)
+print(5. ** 4)
+
+#multiplicacion: '*'
+print(4 * 5)
+print(3.4 * 2)
+
+#division: '/'
+print(20 / 5)
+print(35.8 / 3)
+
+#division entera: '//'
+print(34 // 6)
+print(34.32 // 4)
+
+#modulo: '%'
+print(35 % 4)
+
+#suma: '+'
+print(4 + 3)
+print(3.5 + 5.6)
+
+#resta: '-'
+print(23 - 12)
+print(45.5 - 12.3)
+
+#operadores mas parentesis
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2 + 12)
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+
+#Un operador "unario" es un operador con solo un operando, por ejemplo, -1, o +3.
+#Un operador "binario" es un operador con dos operandos, por ejemplo, 4 + 5, o 12 % 5.
