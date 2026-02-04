deed5f42977aa63f4a94ee29b6d2a4a317322b7e
txarandev <programmingtxaran@gmail.com>
2026-02-04 11:51:51 +0100

    ordenando mejor los archivos del curso y añadiendo mas contenido.

---
+++ Modulo_Inicio/07_operadores_logicos.py
@@ -0,0 +1,30 @@
+#Operadores logicos 
+
+#Operador = Igual a '=='.
+num = 3
+
+print(num == 4) #Imprime False.
+print(num == 3) #Imprime True.
+
+#Operador = No es igual a  '!='.
+print(num != 3) #Imprime False.
+print(num != 4) #Imprime True.
+
+#Operador = Mayor que '>'.
+print(num > 2) #Imprime True.
+print(num > 4) #Imprime False.
+
+#Operador = Mayor o Igual que '>='.
+print(num >= 3) #Imprime True.
+print(num >= 5) #Imprime False.
+
+#Operador = Menor que '<'.
+print(num < 2) #Imprime False.
+print(num < 5) #Imprime True.
+
+#Operador = Menor o Igual que '<='
+print(num <= 3) #Imprime True
+print(num <= 1) #Imprime False
+
+
+
